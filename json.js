661d885b68c9c99cf53bed49b95153299ab7d231
Yazan Masoud <yazanmas895@gmail.com>
2026-02-24 18:36:40 +0100

    Add JSON file for quiz questions and update index.html to include it

---
+++ json.js
@@ -0,0 +1,83 @@
+let questions = [
+    {
+        "question": "Was ist das nächste Zahl in der Reihe? 2, 6, 12, 20, ?",
+        "answer-1": "28",
+        "answer-2": "30",
+        "answer-3": "32",
+        "answer-4": "36",
+        "right-answer": 2
+    },
+
+    {
+        "question": "Ich habe Städte, aber keine Häuser. Ich habe Flüsse, aber kein Wasser. Was bin ich?",
+        "answer-1": "Traum",
+        "answer-2": "Karte",
+        "answer-3": "Buch",
+        "answer-4": "Welt",
+        "right-answer": 2
+    },
+    {
+        "question": "Was ist 15% von 200?",
+        "answer-1": "20",
+        "answer-2": "25",
+        "answer-3": "30",
+        "answer-4": "35",
+        "right-answer": 3
+    },
+    {
+        "question": "Welche Zahl fehlt? 1, 4, 9, 16, ?",
+        "answer-1": "20",
+        "answer-2": "21",
+        "answer-3": "24",
+        "answer-4": "25",
+        "right-answer": 4
+    },
+    {
+        "question": "Wenn alle Bloops Razzies sind und alle Razzies Lazzies sind , sind dann alle Bloops Lazzies?",
+        "answer-1": "Ja",
+        "answer-2": "Nein",
+        "answer-3": "Nur manchmal",
+        "answer-4": "Unklar",
+        "right-answer": 1
+    },
+    {
+        "question": "Was wird größer, je mehr du davon wegnimmst?",
+        "answer-1": "Schatten",
+        "answer-2": "Loch",
+        "answer-3": "Zeit",
+        "answer-4": "Zahl",
+        "right-answer": 2
+    },
+    {
+        "question": "Wie viele Seiten hat ein Würfel?",
+        "answer-1": "4",
+        "answer-2": "5",
+        "answer-3": "6",
+        "answer-4": "8",
+        "right-answer": 3
+    },
+    {
+        "question": "Welche Zahl passt nicht dazu? 3, 5, 7, 9, 11",
+        "answer-1": "3",
+        "answer-2": "5",
+        "answer-3": "7",
+        "answer-4": "9",
+        "right-answer": 4
+    },
+    {
+        "question": "Wenn ein Zug 60 km/h fährt, wie weit kommt er in 30 Minuten?",
+        "answer-1": "15 km",
+        "answer-2": "30 km",
+        "answer-3": "25 km",
+        "answer-4": "30 km",
+        "right-answer": 4
+    },
+    {
+        "question": "Was ist schwerer: 1 kg Federn oder 1 kg Eisen?",
+        "answer-1": "Federn",
+        "answer-2": "Eisen",
+        "answer-3": "Beides gleich",
+        "answer-4": "Kommt drauf an",
+        "right-answer": 3
+    }
+]
