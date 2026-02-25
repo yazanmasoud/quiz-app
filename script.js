e738d9e132878e236f7731b1251ea0c975dbadaa
Yazan Masoud <yazanmas895@gmail.com>
2026-02-25 19:47:35 +0100

    Update question display and navigation; enable next button after answering and reset answer styles

--- script.js
+++ script.js
@@ -7,28 +7,56 @@ let currentQuestion = 0;
 
 function showQuestion() {
     let question = questions[currentQuestion];
-    
+
     document.getElementById('question-1').innerHTML = "Frage: " + question.question;
     document.getElementById('answer-1').innerHTML = question["answer-1"];
     document.getElementById('answer-2').innerHTML = question["answer-2"];
     document.getElementById('answer-3').innerHTML = question["answer-3"];
     document.getElementById('answer-4').innerHTML = question["answer-4"];
+    document.getElementById('current-question-number').innerText =1;
 }
 
 function answer(selection) {
     let question = questions[currentQuestion];
-    let selectedQuestionNumber = selection.slice(0, -1)
+    let selectedQuestionNumber = selection.slice(-1);
+    let selectedId = selection.slice(0, -1);
     let selectRightAnswerNumber = question["right-answer"];
-    
-    let idOfRightAnswer = selectedQuestionNumber + selectRightAnswerNumber;
+    let idOfRightAnswer = selectedId + selectRightAnswerNumber;
 
-    
-    if (question["right-answer"] == selectedQuestionNumber){
+    if (question["right-answer"] == selectedQuestionNumber) {
         document.getElementById(selection).classList.add('bg-success');
-        
-    }else {
+
+    } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
     document.getElementById('next-button').disabled = false;
+}
+
+function showNextQuestion() {
+    currentQuestion += 1;
+    document.getElementById('next-button').disabled = true;
+    resetColoredAnswers();
+    showQuestion();
+
+}
+
+function resetColoredAnswers() {
+
+    document.getElementById('answer-1').parentNode.classList.remove('bg-danger')
+    document.getElementById('answer-1').parentNode.classList.remove('bg-success')
+    document.getElementById('answer-1').classList.remove('bg-success');
+
+
+    document.getElementById('answer-2').parentNode.classList.remove('bg-danger')
+    document.getElementById('answer-2').parentNode.classList.remove('bg-success')
+    document.getElementById('answer-2').classList.remove('bg-success');
+
+    document.getElementById('answer-3').parentNode.classList.remove('bg-danger')
+    document.getElementById('answer-3').parentNode.classList.remove('bg-success')
+    document.getElementById('answer-3').classList.remove('bg-success');
+
+    document.getElementById('answer-4').parentNode.classList.remove('bg-danger')
+    document.getElementById('answer-4').parentNode.classList.remove('bg-success')
+    document.getElementById('answer-4').classList.remove('bg-success');
 }
