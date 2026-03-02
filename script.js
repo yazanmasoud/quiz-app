58ccf13f694cef14ffd07876fd981536276f708d
Yazan Masoud <yazanmas895@gmail.com>
2026-03-02 06:51:54 +0100

    Add end screen functionality and progress bar; enhance styles for better layout

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+let rightAnswers = 0;
+let currentQuestion = 0;
+
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
     showQuestion();
 }
 
-let currentQuestion = 0;
 
 function showQuestion() {
 
@@ -23,33 +25,42 @@ function showQuestion() {
     }
 }
 
+
 function answer(selection) {
     let question = questions[currentQuestion];
     let selectedQuestionNumber = selection.slice(-1);
     let selectedId = selection.slice(0, -1);
     let selectRightAnswerNumber = question["right-answer"];
     let idOfRightAnswer = selectedId + selectRightAnswerNumber;
-    let rightAnswers = 0;
 
     if (question["right-answer"] == selectedQuestionNumber) {
         document.getElementById(selection).classList.add('bg-success');
         rightAnswers++;
-        document.getElementById('end-right-answers').innerHTML += rightAnswers;
 
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
     document.getElementById('next-button').disabled = false;
+    document.getElementById('end-right-answers').innerHTML = rightAnswers;
+    document.getElementById('end-answers').innerHTML = questions.length;
 }
+
 
 function showNextQuestion() {
     currentQuestion += 1;
     document.getElementById('next-button').disabled = true;
     resetColoredAnswers();
     showQuestion();
+    let percent = currentQuestion / questions.length;
+    percent = Math.round(percent * 100);
+
+
+    document.getElementById('progress-bar').innerText = percent + "%";
+    document.getElementById('progress-bar').style.width = percent + "%";
 
 }
+
 
 function resetColoredAnswers() {
 
@@ -69,4 +80,17 @@ function resetColoredAnswers() {
     document.getElementById('answer-4').parentNode.classList.remove('bg-danger')
     document.getElementById('answer-4').parentNode.classList.remove('bg-success')
     document.getElementById('answer-4').classList.remove('bg-success');
+}
+
+
+function restartGame() {
+    document.getElementById('end-screen').style = "display: none";
+    document.getElementById('question-body').style = "";
+    document.getElementById('card-img-top').style = "";
+    currentQuestion = 0;
+    rightAnswers = 0;
+    document.getElementById('progress-bar').innerText = 0;
+    document.getElementById('progress-bar').style.width = 0;
+
+    init();
 }
